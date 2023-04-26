fa3c0f8abed8e3619ceb1ad6911f423e6aacca43
nada <nadasamir9334@gmail.com>
2023-04-26 12:42:01 -0400

    navbar working

--- Components/Nav.js
+++ Components/Nav.js
@@ -32,11 +32,12 @@ import Switch from '@mui/material/Switch';
 import { ColorModeContext } from '../public/theme'
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { LogoDev } from '@mui/icons-material';
+import { LogoDev, Style } from '@mui/icons-material';
 import Image from 'next/image';
 import Styles from '../src/styles/Nav.module.css'
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { NodeNextRequest } from 'next/dist/server/base-http/node';
 
 
 const drawerWidth = 240;
@@ -76,17 +77,9 @@ function Nav() {
     const colorMode = React.useContext(ColorModeContext);
     const theme = useTheme();
     const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [open, setOpen] = useState(false)
     const handleDrawerToggle = () => {
         setMobileOpen((prevState) => !prevState);
-    };
-
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
     };
 
 
@@ -110,47 +103,6 @@ function Nav() {
         }
     }, [])
 
-    const StyledMenu = styled((props) => (
-        <Menu
-            elevation={0}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-            }}
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            {...props}
-        />
-    ))(({ theme }) => ({
-        '& .MuiPaper-root': {
-            borderRadius: 6,
-            marginTop: theme.spacing(1),
-            width: 600,
-            color:
-                theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-            boxShadow:
-                'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-            '& .MuiMenu-list': {
-                padding: '4px 0',
-            },
-            '& .MuiMenuItem-root': {
-                '& .MuiSvgIcon-root': {
-                    fontSize: 18,
-                    color: theme.palette.text.secondary,
-                    marginRight: theme.spacing(1.5),
-                },
-                '&:active': {
-                    backgroundColor: alpha(
-                        theme.palette.primary.main,
-                        theme.palette.action.selectedOpacity,
-                    ),
-                },
-            },
-        },
-    }));
-
 
 
 
@@ -173,63 +125,7 @@ function Nav() {
         </Box>);
 
 
-    // <Box onClick={handleDrawerToggle} sx={{
-    //     textAlign: 'center', backgroundColor: "background.main",
-    //     color: "text.primary", height: "100vh", pt: 3, borderTopRightRadius: "20px", borderBottomRightRadius: "20px",
-    // }}>
 
-    //     <IconButton >
-    //         <CloseIcon sx={{ color: "secondary.main", position: "absolute", left: "90px" }} />
-    //     </IconButton>
-    //     {/* 
-    //     <Typography variant="h6" sx={{ my: 2 }}>
-    //         nada
-    //     </Typography> */}
-
-
-    //     <List sx={{ display: "grid", direction: "column", justifyContent: "center", my: 3 }} data-aos="fade-right" data-aos-once="false" >
-    //         <ListItem>
-    //             <a href="#home">
-    //                 <ListItemButton className={Styles.drawer_icon}>
-    //                     <HomeIcon sx={{ color: "secondary.main", width: "50px" }} />
-    //                     <ListItemText primary="Home" />
-    //                 </ListItemButton>
-    //             </a>
-    //         </ListItem>
-    //         <ListItem>
-    //             <a href="#about">
-    //                 <ListItemButton className={Styles.drawer_icon}>
-    //                     <Person2Icon sx={{ mx: 2 }} />
-    //                     <ListItemText primary="About" />
-    //                 </ListItemButton>
-    //             </a>
-    //         </ListItem>
-    //         <ListItem>
-    //             <a href="#skills">
-    //                 <ListItemButton className={Styles.drawer_icon}>
-    //                     < SensorOccupiedIcon sx={{ mx: 2 }} />
-    //                     <ListItemText primary="Skills" />
-    //                 </ListItemButton>
-    //             </a>
-    //         </ListItem>
-    //         <ListItem>
-    //             <a href="#projects">
-    //                 <ListItemButton className={Styles.drawer_icon}>
-    //                     <WorkIcon sx={{ mx: 2 }} />
-    //                     <ListItemText primary="Projects" />
-    //                 </ListItemButton>
-    //             </a>
-    //         </ListItem>
-    //         <ListItem>
-    //             <a href="#contact">
-    //                 <ListItemButton className={Styles.drawer_icon}>
-    //                     <ConnectWithoutContactIcon sx={{ mx: 2 }} />
-    //                     <ListItemText primary="Contact" />
-    //                 </ListItemButton>
-    //             </a>
-    //         </ListItem>
-    //     </List>
-    // </Box>
 
 
     return (
@@ -295,72 +191,86 @@ function Nav() {
                                 </Box>
 
                             </Box>
-                            <IconButton
-                                id="demo-customized-button"
-                                variant="contained"
-                                disableElevation
-                                onClick={handleClick}
-                                // endIcon={<KeyboardArrowDownIcon />}
-                                color="inherit"
-                                aria-label="open drawer"
-                                // onClick={handleDrawerToggle}
-                                sx={{ display: { md: 'none' }, backgroundColor: "secondary.main", borderRadius: "10px", mt: 1 }}
-                            >
-                                Menu<MenuIcon />
-                            </IconButton>
-                            <StyledMenu
-                                id="demo-customized-menu"
-                                MenuListProps={{
-                                    'aria-labelledby': 'demo-customized-button',
-                                }}
-                                anchorEl={anchorEl}
-                                open={open}
-                                onClose={handleClose}
-                            >
-                                <MenuItem onClick={handleClose} >
-                                    Edit
-                                </MenuItem>
-                                <MenuItem onClick={handleClose} >
 
-                                    Duplicate
-                                </MenuItem>
-                                <Divider sx={{ my: 0.5 }} />
-                                <MenuItem onClick={handleClose} >
-
-                                    Archive
-                                </MenuItem>
-                                <MenuItem onClick={handleClose} >
-
-                                    More
-                                </MenuItem>
-                            </StyledMenu>
 
                         </Toolbar>
                     </Container>
 
                 </AppBar>
+                {/* mobile view */}
+
+                <AppBar variant="permanent" position="fixed" sx={{ display: { md: 'none', sm: 'block' }, backgroundColor: "#212529" }}>
+                    <Container>
+                        <Toolbar >
+                            <Box display='flex' flexGrow={1} >
+                                <Typography
+                                    variant="h4"
+                                    sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' }, color: "secondary.main", mt: 1 }}
+                                >
+                                    Ahmed Fakhry
+                                </Typography>
+                                <IconButton
+                                    variant="contained"
+                                    onClick={() => setOpen(!open)}
+                                    sx={{ display: { md: 'none', sm: 'block' }, backgroundColor: "secondary.main", borderRadius: "10px", mt: 1, display: "flex" }}
+                                    className={Styles.menuButton}
+                                >
+                                    <Typography variant='h6' sx={{ color: "black" }}>Menu</Typography>
+
+                                    <MenuIcon sx={{ color: "black" }} />
+                                </IconButton>
+                            </Box>
+                        </Toolbar>
+                        <Box>
+                            {open && (
+                                <List >
+                                    <ListItem>
+                                        <a href="#about">
+                                            <ListItemText primary="ABOUT" />
+                                        </a>
+                                    </ListItem>
+                                    <ListItem>
+                                        <a href="#skills">
+                                            <ListItemText primary="SKILLS" />
+                                        </a>
+                                    </ListItem>
+                                    <ListItem>
+                                        <a href="#projects">
+                                            <ListItemText primary="PROJECTS" />
+                                        </a>
+                                    </ListItem>
+                                    <ListItem>
+                                        <a href="#papers">
+                                            <ListItemText primary="PAPERS" />
+                                        </a>
+                                    </ListItem>
+                                    <ListItem>
+                                        <a href="#projects">
+                                            <ListItemText primary="TIMELINE" />
+                                        </a>
+                                    </ListItem>
+                                    <ListItem>
+                                        <a href="#contact">
+                                            <ListItemText primary="Contact" />
+                                        </a>
+                                    </ListItem>
+                                    <ListItem>
+                                        <a href="#contact">
+                                            <ListItemText primary="RESUME" />
+                                        </a>
+                                    </ListItem>
+
+
+                                </List>
+
+                            )}
+
+                        </Box>
+                    </Container>
+                </AppBar>
 
                 {/* </HideOnScroll> */}
-                <Box component="nav">
-                    <Drawer
-                        // container={container}
-                        variant="temporary"
-                        open={mobileOpen}
-                        onClose={handleDrawerToggle}
-                        ModalProps={{
-                            keepMounted: true, // Better open performance on mobile.
-                        }}
-                        sx={{
-                            display: { xs: 'block', sm: 'block', md: 'none' },
-                            '& .MuiDrawer-paper': {
-                                boxSizing: 'border-box', width: drawerWidth,
-                                borderTopRightRadius: "20px", borderBottomRightRadius: "20px", boxShadow: "none"
-                            },
-                        }}
-                    >
-                        {drawer}
-                    </Drawer>
-                </Box>
+
 
             </Box >
             {/* <ScrollTop {...props}>
@@ -376,5 +286,62 @@ function Nav() {
 
 
 
-export default Nav
+export default Nav// <Box onClick={handleDrawerToggle} sx={{
+    //     textAlign: 'center', backgroundColor: "background.main",
+    //     color: "text.primary", height: "100vh", pt: 3, borderTopRightRadius: "20px", borderBottomRightRadius: "20px",
+    // }}>
+
+    //     <IconButton >
+    //         <CloseIcon sx={{ color: "secondary.main", position: "absolute", left: "90px" }} />
+    //     </IconButton>
+    //     {/*
+    //     <Typography variant="h6" sx={{ my: 2 }}>
+    //         nada
+    //     </Typography> */}
+
+
+    //     <List sx={{ display: "grid", direction: "column", justifyContent: "center", my: 3 }} data-aos="fade-right" data-aos-once="false" >
+    //         <ListItem>
+    //             <a href="#home">
+    //                 <ListItemButton className={Styles.drawer_icon}>
+    //                     <HomeIcon sx={{ color: "secondary.main", width: "50px" }} />
+    //                     <ListItemText primary="Home" />
+    //                 </ListItemButton>
+    //             </a>
+    //         </ListItem>
+    //         <ListItem>
+    //             <a href="#about">
+    //                 <ListItemButton className={Styles.drawer_icon}>
+    //                     <Person2Icon sx={{ mx: 2 }} />
+    //                     <ListItemText primary="About" />
+    //                 </ListItemButton>
+    //             </a>
+    //         </ListItem>
+    //         <ListItem>
+    //             <a href="#skills">
+    //                 <ListItemButton className={Styles.drawer_icon}>
+    //                     < SensorOccupiedIcon sx={{ mx: 2 }} />
+    //                     <ListItemText primary="Skills" />
+    //                 </ListItemButton>
+    //             </a>
+    //         </ListItem>
+    //         <ListItem>
+    //             <a href="#projects">
+    //                 <ListItemButton className={Styles.drawer_icon}>
+    //                     <WorkIcon sx={{ mx: 2 }} />
+    //                     <ListItemText primary="Projects" />
+    //                 </ListItemButton>
+    //             </a>
+    //         </ListItem>
+    //         <ListItem>
+    //             <a href="#contact">
+    //                 <ListItemButton className={Styles.drawer_icon}>
+    //                     <ConnectWithoutContactIcon sx={{ mx: 2 }} />
+    //                     <ListItemText primary="Contact" />
+    //                 </ListItemButton>
+    //             </a>
+    //         </ListItem>
+    //     </List>
+    // </Box>
+
 
